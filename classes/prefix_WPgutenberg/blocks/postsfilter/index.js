b4fa068a3ecf660de2200c28acf5da7931dc9737
david-gap <david.voglgsang@gmail.com>
2021-11-11 12:29:41 +0100

    💎 IMPROVE: taxonomy strings, block extension

--- classes/prefix_WPgutenberg/blocks/postsfilter/index.js
+++ classes/prefix_WPgutenberg/blocks/postsfilter/index.js
@@ -408,9 +408,7 @@ export default registerBlockType( 'templates/postsfilter', {
                     <a href="#">
                         {PostImg(postThumb, postTaxonomyFilterOptions, post.id, media[ post.id ])}
                         <div class="post-content">
-                          <h4>
-                            {PostValues(postTextOne, post, postTaxonomyFilterOptions, "link_row1", taxOne)}
-                          </h4>
+                          {PostValues(postTextOne, post, postTaxonomyFilterOptions, "link_row1", taxOne)}
                           {PostValues(postTextTwo, post, postTaxonomyFilterOptions, "link_row2", taxTwo)}
                         </div>
                     </a>
